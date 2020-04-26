d4a9d0d6afaf9ed587868269e6fabeb556cc1e7c
Hamid Hosseinzadeh <hamidhszd@gmail.com>
2020-04-26 17:14:27 +0430

    added a simple help page

--- MySecondDjangoProject/MySecondDjangoApp_Return_HTML/views.py
+++ MySecondDjangoProject/MySecondDjangoApp_Return_HTML/views.py
@@ -4,3 +4,7 @@ from django.http import HttpResponse
 # Create your views here.
 def index(request):
     return HttpResponse("<H1> This is my second project and my second APP that returns a text in H1 tag </h1>")
+
+def help(request):
+    c = {'my_context' : 'This is the context from views.py'}
+    return render(request, "MySecondDjangoApp_Return_HTML/help.html",context=c)
