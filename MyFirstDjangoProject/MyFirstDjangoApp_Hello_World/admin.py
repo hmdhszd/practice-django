4adecc6eee8b44b5b5df883920d0e5f94c2688d9
Hamid Hosseinzadeh <hamidhszd@gmail.com>
2020-04-28 10:29:24 +0430

    created some models and added to admin page

--- MyFirstDjangoProject/MyFirstDjangoApp_Hello_World/admin.py
+++ MyFirstDjangoProject/MyFirstDjangoApp_Hello_World/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
-
+from MyFirstDjangoApp_Hello_World.models import Topic,AccessRecord,Webpage
 # Register your models here.
+admin.site.register(Topic)
+admin.site.register(AccessRecord)
+admin.site.register(Webpage)
