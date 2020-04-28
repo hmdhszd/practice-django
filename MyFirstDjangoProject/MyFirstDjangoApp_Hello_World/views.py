7c2d1148ce68adbfdfe2c848345ea3a567da1e82
Hamid Hosseinzadeh <hamidhszd@gmail.com>
2020-04-28 13:44:23 +0430

    added the data into main HTML page

--- MyFirstDjangoProject/MyFirstDjangoApp_Hello_World/views.py
+++ MyFirstDjangoProject/MyFirstDjangoApp_Hello_World/views.py
@@ -1,9 +1,11 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from MyFirstDjangoApp_Hello_World.models import Topic,AccessRecord,Webpage
 
 # Create your views here.
 
 def index(request):
-    mydict = {'insert_me':'hello im the INSERT ME   from  Views.py'}
-    return render(request, 'MyFirstDjangoApp_Hello_World/index.html' , context=mydict)
+    webpages_list = AccessRecord.objects.order_by('date')
+    datedicts = {"access_records":webpages_list}
+    return render(request, 'MyFirstDjangoApp_Hello_World/index.html' , context=datedicts)
 
