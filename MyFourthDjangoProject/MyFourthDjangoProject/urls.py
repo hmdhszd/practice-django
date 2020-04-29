08a20645828c3466912d07437056f3a982b78960
Hamid Hosseinzadeh <hamidhszd@gmail.com>
2020-04-29 14:47:41 +0430

    added data from model to html page

--- MyFourthDjangoProject/MyFourthDjangoProject/urls.py
+++ MyFourthDjangoProject/MyFourthDjangoProject/urls.py
@@ -14,8 +14,12 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path,include
+from app4 import views
+
 
 urlpatterns = [
+    path('', views.index, name='index'), 
+    path('users/', include('app4.urls')), 
     path('admin/', admin.site.urls),
 ]
--- MyFourthDjangoProject/app4/models.py
+++ MyFourthDjangoProject/app4/models.py
@@ -7,7 +7,6 @@ class User(models.Model):
     last_name = models.CharField(max_length=50)
     email = models.EmailField(unique=True, max_length=254)
 
-    des __str__(self):
-        return self.last_name, self.last_name
-    
-    
+    def __str__(self):
+        full_name = self.first_name + " " + self.last_name
+        return full_name
