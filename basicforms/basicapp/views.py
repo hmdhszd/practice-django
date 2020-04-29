acb4aead5275db098cc992bb208d4f7e4cdd955c
Hamid Hosseinzadeh <hamidhszd@gmail.com>
2020-04-29 19:34:14 +0430

    add bootstrap/ get data from form and print in terminal

--- basicforms/basicapp/views.py
+++ basicforms/basicapp/views.py
@@ -9,4 +9,22 @@ def index(resuest):
 
 def form_name_view(request):
     form = forms.FormName()
+    
+    
+    
+    
+    
+    
+    if request.method == 'POST':
+        form = forms.FormName(request.POST)
+               
+        if form.is_valid():
+            print('validation successful')
+            print(form.cleaned_data['name'])
+            
+    
+    
+    
+    
+    
     return render(request, 'basicapp/form_page.html', {'form':form})
