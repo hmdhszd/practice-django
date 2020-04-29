c62499321b368e66eabb5a91d05ab1d8ab656de1
Hamid Hosseinzadeh <hamidhszd@gmail.com>
2020-04-29 12:37:34 +0430

    created fourth project

---
+++ MyFourthDjangoProject/app4/models.py
@@ -0,0 +1,13 @@
+from django.db import models
+
+
+# Create your models here.
+class User(models.Model):
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
+    email = models.EmailField(unique=True, max_length=254)
+
+    des __str__(self):
+        return self.last_name, self.last_name
+    
+    
