f3e16e93eb0edafb93e6bb3bc2d36026dbb28e4f
Hamid Hosseinzadeh <hamidhszd@gmail.com>
2020-05-16 18:38:46 +0430

    Created DetailView and ListView

--- NEW_PROJECT__class_based_views/basic_app/views.py
+++ NEW_PROJECT__class_based_views/basic_app/views.py
@@ -1,8 +1,37 @@
 from django.shortcuts import render
-from django.views.generic import View,TemplateView
+from django.views.generic import View,TemplateView,ListView,DetailView
 from django.http import HttpResponse
+from . import models
 
 # Create your views here.
+
+
+class SchoolListView(ListView):
+    context_object_name = 'schools'
+    model = models.School
+
+
+class SchoolDetailView(DetailView):
+    context_object_name = 'school_detail'
+    model = models.School
+    template_name = 'basic_app/school_detail.html'
+
+
+
+
+
+
+class IndexView(TemplateView):
+    template_name = 'index.html'
+    
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['injectme'] = 'basic injection data !'
+        return context
+
+
+
+
 
 # def index(request):
 #     return render(request,'index.html')
@@ -17,10 +46,3 @@ from django.http import HttpResponse
 
 
 
-class IndexView(TemplateView):
-    template_name = 'index.html'
-    
-    def get_context_data(self, **kwargs):
-        context = super().get_context_data(**kwargs)
-        context['injectme'] = 'basic injection data !'
-        return context
