36023502369e5c75cc794f87c97dbd1c1a9a17ae
Hamid Hosseinzadeh <hamidhszd@gmail.com>
2020-05-16 14:46:54 +0430

    inject data from TemplateView to Index.html

--- NEW_PROJECT__class_based_views/basic_app/views.py
+++ NEW_PROJECT__class_based_views/basic_app/views.py
@@ -19,3 +19,8 @@ from django.http import HttpResponse
 
 class IndexView(TemplateView):
     template_name = 'index.html'
+    
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['injectme'] = 'basic injection data !'
+        return context
