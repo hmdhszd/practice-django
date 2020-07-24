bcb6ca5b1e99984c2aea0a28dccdf31bcedf62a8
Hamid Hosseinzadeh <hamidhszd@gmail.com>
2020-07-24 16:21:20 +0430

    fix bugs

--- learn_users_django/basic_app/urls.py
+++ learn_users_django/basic_app/urls.py
@@ -8,5 +8,6 @@ app_name = 'basic_app'
 urlpatterns = [
     path('register/', views.register, name='register'),
     path('user_login/', views.user_login, name='user_login'),
+    path('user_logout/', views.user_logout, name='user_logout'),
 
 ]
